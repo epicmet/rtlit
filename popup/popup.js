6e3d72b836811834011b31869251b3910dde3589
Mahdi Aghaei <mahd.aghaei@gmail.com>
2023-08-08 23:50:31 +0330

    feat: Basic popup page design :art:

--- popup/popup.js
+++ popup/popup.js
@@ -1,9 +1,11 @@
 const goToOptionsButton = document.querySelector("#go-to-options");
 
-goToOptionsButton.addEventListener("click", () => {
-  if (chrome.runtime.openOptionsPage) {
-    chrome.runtime.openOptionsPage();
-  } else {
-    window.open(chrome.runtime.getURL("options/options.html"));
-  }
-});
+if (goToOptionsButton) {
+  goToOptionsButton.addEventListener("click", () => {
+    if (chrome.runtime.openOptionsPage) {
+      chrome.runtime.openOptionsPage();
+    } else {
+      window.open(chrome.runtime.getURL("options/options.html"));
+    }
+  });
+}
