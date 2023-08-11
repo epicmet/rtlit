57d698b9bf87bc38f45d01f6f8d2dd8e7391b558
Mahdi Aghaei <mahd.aghaei@gmail.com>
2023-08-11 19:56:14 +0330

    feat: Disable options page for now

--- popup/js/index.js
+++ popup/js/index.js
@@ -77,14 +77,17 @@ blacklistedWebsites.forEach((address) => {
 
 const goToOptionsButton = document.querySelector("#go-to-options");
 
+// Hide setting icon for now
 if (goToOptionsButton) {
-  goToOptionsButton.addEventListener("click", () => {
-    if (chrome.runtime.openOptionsPage) {
-      chrome.runtime.openOptionsPage();
-    } else {
-      window.open(chrome.runtime.getURL("options/options.html"));
-    }
-  });
+  goToOptionsButton.style.display = "none";
+
+  // goToOptionsButton.addEventListener("click", () => {
+  //   if (chrome.runtime.openOptionsPage) {
+  //     chrome.runtime.openOptionsPage();
+  //   } else {
+  //     window.open(chrome.runtime.getURL("options/options.html"));
+  //   }
+  // });
 }
 
 const automaticRtlOn = document.querySelector(".toggle-autortl .on");
