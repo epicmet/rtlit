292d0d5be729f04cb2b566a53054c1f424f59e20
Mahdi Aghaei <mahd.aghaei@gmail.com>
2023-08-11 14:59:04 +0330

    feat: Made toggle autoRtl buttons work visually

--- popup/popup.js
+++ popup/popup.js
@@ -9,3 +9,20 @@ if (goToOptionsButton) {
     }
   });
 }
+
+const automaticRtlOn = document.querySelector(".toggle-autortl .on");
+const automaticRtlOff = document.querySelector(".toggle-autortl .off");
+
+automaticRtlOn.addEventListener("click", () => {
+  chrome.storage.sync.set({ automaticRtl: true }, () => {
+    automaticRtlOn.classList.add("active");
+    automaticRtlOff.classList.remove("active");
+  });
+});
+
+automaticRtlOff.addEventListener("click", () => {
+  chrome.storage.sync.set({ automaticRtl: false }, () => {
+    automaticRtlOff.classList.add("active");
+    automaticRtlOn.classList.remove("active");
+  });
+});
