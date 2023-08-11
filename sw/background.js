cb03aa992b3ba28d4ebb7fac4d36ff369cb13ad3
Mahdi Aghaei <mahd.aghaei@gmail.com>
2023-08-11 19:47:34 +0330

    feat: New icon! Also removed badge

--- sw/background.js
+++ sw/background.js
@@ -1,15 +1,3 @@
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ automaticRtl: true, blacklist: [] }, () => {
-    chrome.action.setBadgeText({
-      text: "ON",
-    });
-  });
-});
-
-chrome.storage.onChanged.addListener((changes) => {
-  if (changes.automaticRtl) {
-    chrome.action.setBadgeText({
-      text: changes.automaticRtl.newValue ? "ON" : "OFF",
-    });
-  }
+  chrome.storage.sync.set({ automaticRtl: true, blacklist: [] }, () => {});
 });
