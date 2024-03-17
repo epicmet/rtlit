785d44862bd5e1ebcf4e1732bd644540018a22b4
Mahdi Aghaei <mahd.aghaei@gmail.com>
2024-03-17 17:06:47 +0330

    WIP: Generate manifest.json based on browser

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,10 +1,45 @@
-import fs from "node:fs/promises";
+import fsp from "node:fs/promises";
 
 import * as esbuild from "esbuild";
 import copy from "esbuild-plugin-copy";
 
 // TODO: Make an abstraction of all these common use cases.
 // Like logging, running jobs and build steps.
+
+/**
+ * Manifest v3 types -> https://json.schemastore.org/chrome-manifest
+ *
+ * @typedef {Object} ContentScripts
+ * @property {string[]} js
+ * @property {string[]} matches
+ *
+ * @typedef {Object} Icons
+ * @property {string} 16
+ * @property {string} 32
+ * @property {string} 48
+ * @property {string} 128
+ *
+ * @typedef {Object} Background
+ * @property {string} service_worker
+ *
+ * @typedef {Object} Action
+ * @property {string} default_popup
+ *
+ * @typedef {Object} Manifest
+ * @property {string} manifest_version
+ * @property {string} name
+ * @property {string} description
+ * @property {string} version
+ * @property {ContentScripts[]} content_scripts
+ * @property {string[]} permissions
+ * @property {Icons} icons
+ * @property {Background} background
+ * @property {Action} action
+ */
+
+/**
+ * @typedef {"chrome"|"firefox"} TargetBrowser
+ */
 
 // *** Utilities *** //
 function getBoolEnv(key) {
@@ -21,6 +56,11 @@ function log(...meta) {
 
 // *** Args *** //
 const watchMode = getBoolEnv("ESB_WATCH");
+const targetBrowser = process.env.ESB_TARGET_BROWSER;
+if (!targetBrowser) {
+  print("error", "Please specify the target browser via ESB_TARGET_BROWSER environment variable");
+  process.exit(1);
+}
 
 function onExit(fn) {
   /**
@@ -51,6 +91,81 @@ function onExit(fn) {
 
 // TODO: Functoins for moving things (html, and manifest.json)
 
+class ManifestTrasnformer {
+  /**
+   * @type {Manifest}
+   */
+  rawManifest = undefined;
+
+  /**
+   * @type {TargetBrowser}
+   */
+  target = undefined;
+
+  /**
+   * @param {Manifest} rawManifest
+   * @param {TargetBrowser} target
+   */
+  constructor(rawManifest) {
+    this.rawManifest = rawManifest;
+  }
+
+  /**
+   * @param {TargetBrowser} target
+   */
+  build(target) {
+    return this[target]();
+  }
+
+  firefox() {
+    const fManifest = this.rawManifest
+
+    fManifest.background = {
+      scripts: ["sw/index.js"]
+    }
+
+    fManifest.browser_specific_settings = {
+      gecko: {
+        id: "mahd@gmail.com"
+      }
+    }
+
+    return fManifest;
+  }
+
+  chrome() {
+    const cManifest = this.rawManifest
+
+    cManifest.background = {
+      service_worker: "sw/index.js"
+    }
+
+    return cManifest;
+  }
+}
+
+/**
+ * @param {TargetBrowser} target
+ * @returns {esbuild.Plugin}
+ */
+function createManifestPlugin(target) {
+  return {
+    name: "plugin:create-manifest",
+    setup(build) {
+      build.onEnd(async (_result) => {
+        /**
+         * @type {Manifest}
+         */
+        const rawManifest = JSON.parse(await fsp.readFile("./manifest.json", { encoding: "utf8" }));
+
+        const manifest = new ManifestTrasnformer(rawManifest).build(target);
+
+        await fsp.writeFile(`${TARGET_PATH}/manifest.json`, JSON.stringify(manifest, null, 2));
+      });
+    },
+  }
+}
+
 const TARGET_PATH = "build";
 
 /**
@@ -71,12 +186,15 @@ const buildOptions = {
   plugins: [
     copy({
       resolveFrom: "cwd",
-      assets: {
-        from: ['./src/images/*'],
-        to: [`./${TARGET_PATH}/images`]
-      },
+      assets: [
+        {
+          from: ['./src/images/*'],
+          to: [`./${TARGET_PATH}/images`]
+        },
+      ],
       watch: watchMode
-    })
+    }),
+    createManifestPlugin(targetBrowser),
   ],
 };
 
